da2886fd18fd42ab6cfce263ab9e1afe782d7499
kapsolas <dimitrios@gmail.com>
2014-08-15 19:52:43 -0500

    network infusion update
    network infusion updated to include wlan2 ip address

--- components/system/network/small_tile.php
+++ components/system/network/small_tile.php
@@ -2,12 +2,14 @@
 <div style='text-align: right'><a href='#' class="refresh" onclick='refresh_network()'> </a></div> 
 
 Wlan0 <?= wlan0_status() == true ? '<font color="lime">Enabled</font>.&nbsp; | <a href="#sys/network/disable/wlan0/refresh_network">Disable</a>' : '<font color="red">Disabled</font>. | <a href="#sys/network/enable/wlan0/refresh_network">Enable</a>';  ?><br />
-Wlan1 <?= wlan1_status() == true ? '<font color="lime">Enabled</font>.&nbsp; | <a href="#sys/network/disable/wlan1/refresh_network">Disable</a>' : '<font color="red">Disabled</font>. | <a href="#sys/network/enable/wlan1/refresh_network">Enable</a>';  ?><br /> <br />
+Wlan1 <?= wlan1_status() == true ? '<font color="lime">Enabled</font>.&nbsp; | <a href="#sys/network/disable/wlan1/refresh_network">Disable</a>' : '<font color="red">Disabled</font>. | <a href="#sys/network/enable/wlan1/refresh_network">Enable</a>';  ?><br />
+Wlan2 <?= wlan2_status() == true ? '<font color="lime">Enabled</font>.&nbsp; | <a href="#sys/network/disable/wlan2/refresh_network">Disable</a>' : '<font color="red">Disabled</font>. | <a href="#sys/network/enable/wlan2/refresh_network">Enable</a>';  ?><br /> <br />  
 
 <div id='internet_ip'>Internet IP: <a href='JAVASCRIPT: display_internet()'>Show</a></div><br />
 
 LAN: <?=get_lan()?><br />
 Wlan1: <?=get_wlan1()?><br />
+Wlan2: <?=get_wlan2()?><br />
 Mobile: <?=get_mobile()?><br />
 
 
@@ -45,6 +47,15 @@ function wlan1_status(){
   return false;
 }
 
+function wlan2_status(){                                                       
+  $state = exec("ifconfig wlan2 | grep UP | awk '{print $1}'");                
+    if($state == "UP"){                                                          
+        return true;                                                               
+    }                                                                            
+    return false;                                                                
+}                                                                              
+              
+
 function get_lan(){
   $ip = trim(exec("ifconfig br-lan | grep 'inet' | cut -d: -f2 | awk '{ print $1 }'"));
   if(empty($ip)){
@@ -62,6 +73,15 @@ function get_wlan1(){
     return $ip;
   } 
 }
+
+function get_wlan2(){                                                                                                                                                                                      
+  $ip = trim(exec("ifconfig wlan2 | grep 'inet' | cut -d: -f2 | awk '{ print $1 }'"));                                                                                                                     
+    if(empty($ip)){                                                                                                                                                                                          
+        return 'N/A';                                                                                                                                                                                          
+    }else{                                                                                                                                                                                                   
+        return $ip;                                                                                                                                                                                            
+    }                                                                                                                                                                                                        
+}  
 
 function get_mobile(){
   $ip = trim(exec("ifconfig 3g-wan2 | grep 'inet' | cut -d: -f2 | awk '{ print $1 }'"));
